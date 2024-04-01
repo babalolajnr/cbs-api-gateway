d644855f0f87139bfe714ff0c93d04ce9552c362
Abdulqudduus Babalola <babalolajnr@gmail.com>
2024-04-01 11:45:22 +0100

    Update proxyToFineract function signature

--- src/gateway/gateway.controller.ts
+++ src/gateway/gateway.controller.ts
@@ -1,7 +1,9 @@
 import { All, Controller, Req, Res } from '@nestjs/common';
+import { Request, Response } from 'express';
 
 @Controller('gateway')
 export class GatewayController {
   @All('*')
-  proxyToFineract(@Req() req, @Res() res) {}
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  proxyToFineract(@Req() req: Request, @Res() res: Response) {}
 }
